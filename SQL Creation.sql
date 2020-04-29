97216234017f6174dd0668a078e901cb3ef8d61e
Daniel Truong <danieltruong98@gmail.com>
2020-04-29 14:23:06 -0700

    SQL Creation Script

---
+++ SQL Creation.sql
@@ -0,0 +1,37 @@
+CREATE DATABASE trippity;
+
+USE trippity;
+
+CREATE TABLE `countries` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+ALTER TABLE countries
+ADD CONSTRAINT UI_Countries UNIQUE (name);
+
+CREATE TABLE `trips` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `description` text,
+  `image` varchar(1000) NOT NULL,
+  `country_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_country_idx` (`country_id`),
+  CONSTRAINT `fk_country` FOREIGN KEY (`country_id`) REFERENCES `countries` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+ALTER TABLE trips
+ADD CONSTRAINT UI_Trips UNIQUE (name);
+
+CREATE TABLE `trip_cities` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) DEFAULT NULL,
+  `datetime_of_arrival` datetime DEFAULT NULL,
+  `datetime_of_departure` datetime DEFAULT NULL,
+  `trip_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_trips_idx` (`trip_id`),
+  CONSTRAINT `fk_trips` FOREIGN KEY (`trip_id`) REFERENCES `trips` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
