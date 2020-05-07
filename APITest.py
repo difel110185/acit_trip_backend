3bc4011f3fac32f850d08a8efa006a77cec981e8
AlexVisca <alexvisca@gmail.com>
2020-05-06 17:43:02 -0700

    API test additions

--- APITest.py
+++ APITest.py
@@ -6,7 +6,11 @@ class TestAPI(unittest.TestCase):
     #Input: Nothing
     #Output: List of Dictionaries Containing Trip Information
     #Test: Does the Format equal what we want
-    def test_getAllTrips(self):
+    def test_getTrips(self):
+        """
+        Strips keys from input dict and mock dict, and asserts keys match
+        TODO: Match value data types
+        """
         type_dict = {
             "id": 0,
             "name": "string",
@@ -21,38 +25,49 @@ class TestAPI(unittest.TestCase):
                 }
             ]
         }
+
         trip_list = app.get_trips()
-        self.assertDictEqual(trip_list, type_dict)
+        trip_dict = trip_list[0]
+
+        key_chain = []
+        for key in trip_dict.keys:
+            key_chain.append(key)
+
+        data_chain = []
+        for key in type_dict.keys():
+            data_chain.append(key)
+
+        self.assertEqual(key_chain, data_chain)
 
     #Input: Trip Information
     #Output: Nothing
     #Test: Does the Input Provide the Necessary Information in the Correct Format
     def test_createTrip(self):
-        response = app.create_trip()
-        self.assertTrue(response.status_code == 201)
+        status_code = app.create_trip()
+        self.assertEqual(status_code, 201)
 
 
     #Input: ID of a Single Trip
     #Output: Trip Information for the Trip with the Given ID
     #Test: Is the Outputted Information that of Trip with the Given ID
-    def test_GettingTrip(self):
-        """Response body"""
-        response = app.get_trip()
-        self.assertTrue(response.status_code == 200)
+    def test_getTrip(self):
+        """TODO Check Response for type str"""
+        response, status_code = app.get_trip()
+        self.assertEqual(status_code, 200)
 
     #Input: ID of a Trip & Information
     #Output: Nothing
     #Test: Is the Info in Mysql changed for the Trip with the Given ID
     def test_UpdateTrip(self):
-        response = app.update_trip()
-        self.assertTrue(response.status_code == 200)
+        response, status_code = app.update_trip()
+        self.assertEqual(status_code, 200)
 
     #Input: ID of a Trip
     #Output: Nothing
     #Test: Does the Mysql with Trip of given ID Exist?
     def test_DeleteTrip(self):
-        response = app.delete_trip()
-        self.assertTrue(response.status_code == 200)
+        response, status_code = app.delete_trip()
+        self.assertEqual(status_code, 200)
 
 
 if __name__ == '__main__':
